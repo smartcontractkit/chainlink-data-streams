1eed52ee79ee405187a363c31f856c0eb0f69084
Sam Davies <samsondav@protonmail.com>
2024-07-01 14:34:45 -0400

    Revert "Observations that violate bid<=mid<=ask are invalid (#61)"
    This reverts commit a4b7359e15809b077ae5ec6dcba764c63b388dce.

--- mercury/v3/mercury.go
+++ mercury/v3/mercury.go
@@ -166,12 +166,7 @@ func (rp *reportingPlugin) Observation(ctx context.Context, repts types.ReportTi
 	}
 
 	if bpErr == nil && bidErr == nil && askErr == nil {
-		if err := validatePrices(obs.Bid.Val, obs.BenchmarkPrice.Val, obs.Ask.Val); err != nil {
-			rp.logger.Errorw("Cannot generate price observation: invalid bid/mid/ask", "err", err)
-			p.PricesValid = false
-		} else {
-			p.PricesValid = true
-		}
+		p.PricesValid = true
 	}
 
 	var maxFinalizedTimestampErr error
@@ -230,13 +225,6 @@ func (rp *reportingPlugin) Observation(ctx context.Context, repts types.ReportTi
 	return proto.Marshal(&p)
 }
 
-func validatePrices(bid, benchmarkPrice, ask *big.Int) error {
-	if bid.Cmp(benchmarkPrice) > 0 || benchmarkPrice.Cmp(ask) > 0 {
-		return fmt.Errorf("invariant violated: expected bid<=mid<=ask, got bid: %s, mid: %s, ask: %s", bid, benchmarkPrice, ask)
-	}
-	return nil
-}
-
 func parseAttributedObservation(ao types.AttributedObservation) (PAO, error) {
 	var pao parsedAttributedObservation
 	var obs MercuryObservationProto
@@ -260,13 +248,6 @@ func parseAttributedObservation(ao types.AttributedObservation) (PAO, error) {
 		pao.Ask, err = mercury.DecodeValueInt192(obs.Ask)
 		if err != nil {
 			return parsedAttributedObservation{}, fmt.Errorf("ask cannot be converted to big.Int: %s", err)
-		}
-		if err := validatePrices(pao.Bid, pao.BenchmarkPrice, pao.Ask); err != nil {
-			// NOTE: since nodes themselves are not supposed to set
-			// PricesValid=true if this invariant is violated, this indicates a
-			// faulty/misbehaving node and the entire observation should be
-			// ignored
-			return parsedAttributedObservation{}, fmt.Errorf("observation claimed to be valid, but contains invalid prices: %w", err)
 		}
 		pao.PricesValid = true
 	}
