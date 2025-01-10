7a800432498642f20a785c3cde6cdf5db3d0f020
Bruno Moura <brunotm@gmail.com>
2025-01-10 16:01:23 +0000

    rpc: grpc client basic test

--- rpc/rpc_test.go
+++ rpc/rpc_test.go
@@ -3,27 +3,50 @@ package rpc
 import (
 	context "context"
 	"crypto/ed25519"
+	"net"
 	"testing"
 
 	"github.com/smartcontractkit/chainlink-data-streams/rpc/mtls"
+	"github.com/stretchr/testify/assert"
 	"google.golang.org/grpc"
 )
 
 func TestClient(t *testing.T) {
-	priv, pub := ed25519.GenerateKey(nil)
+	spub, spriv, err := ed25519.GenerateKey(nil)
+	assert.NoError(t, err)
+	cpub, cpriv, err := ed25519.GenerateKey(nil)
+	assert.NoError(t, err)
 
-	c, _ := mtls.NewTransportCredentials(priv, []ed25519.PublicKey{pub})
-	clientConn, _ := grpc.NewClient("", grpc.WithTransportCredentials(c))
+	sMtls, err := mtls.NewTransportCredentials(spriv, []ed25519.PublicKey{cpub})
+	assert.NoError(t, err)
+	s := grpc.NewServer(grpc.Creds(sMtls))
+	srv := &server{}
+	RegisterMercuryServer(s, srv)
+	conn, err := net.Listen("tcp", "127.0.0.1:8080")
+	assert.NoError(t, err)
+	go func() {
+		err := s.Serve(conn)
+		if err != grpc.ErrServerStopped {
+			assert.NoError(t, err)
+		}
+	}()
+
+	cMtls, err := mtls.NewTransportCredentials(cpriv, []ed25519.PublicKey{spub})
+	assert.NoError(t, err)
+	clientConn, err := grpc.NewClient("127.0.0.1:8080", grpc.WithTransportCredentials(cMtls))
+	assert.NoError(t, err)
 	client := NewMercuryClient(clientConn)
 
-	client.Transmit()
+	r, err := client.Transmit(context.Background(), &TransmitRequest{})
+	assert.NoError(t, err)
+
+	assert.NotNil(t, r)
 }
 
 type server struct {
-	*UnimplementedMercuryServer
+	UnimplementedMercuryServer
 }
 
 func (s *server) Transmit(context.Context, *TransmitRequest) (*TransmitResponse, error) {
-
-	return nil, nil
+	return &TransmitResponse{}, nil
 }
