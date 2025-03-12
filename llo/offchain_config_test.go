ab5d35236de4f50c49cf75bb4d66d4eb31aca63b
Sam <samsondav@protonmail.com>
2025-03-12 11:10:08 -0400

    Temporarily suppress errors decoding bad offchain config (#122)

--- llo/offchain_config_test.go
+++ llo/offchain_config_test.go
@@ -1,6 +1,7 @@
 package llo
 
 import (
+	"encoding/hex"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -8,6 +9,24 @@ import (
 )
 
 func Test_OffchainConfig(t *testing.T) {
+	t.Run("decoding invalid bytes", func(t *testing.T) {
+		b, err := hex.DecodeString("7b2265787069726174696f6e57696e646f77223a38363430302c2262617365555344466565223a22302e3332227d")
+		require.NoError(t, err)
+		cfgDecoded, err := DecodeOffchainConfig(b)
+		// HACK: We have actual invalid bytes written on-chain, which we have
+		// to handle to be compatible with older builds which ignored offchain
+		// config.
+		//
+		// FIXME: Return error instead after v0 is fully decomissioned and all
+		// contracts have been updated with proper v1 config.
+		//
+		// MERC-2272
+		require.NoError(t, err)
+		assert.Equal(t, OffchainConfig{
+			ProtocolVersion:                     0,
+			DefaultMinReportIntervalNanoseconds: 0,
+		}, cfgDecoded)
+	})
 	t.Run("version 0", func(t *testing.T) {
 		t.Run("decodes empty offchainconfig (version 0)", func(t *testing.T) {
 			cfgDecoded, err := DecodeOffchainConfig([]byte{})
